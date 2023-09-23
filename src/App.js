f1dfc62f1df90390a01d4831b79d62b2b2ef4abb
DerickPascual <derickvanpascual@gmail.com>
2023-09-23 02:53:18 -0500

    t

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React here for KB
+          Learn React here for Gabe
         </a>
       </header>
     </div>
