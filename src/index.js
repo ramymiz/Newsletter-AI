0133149f06731991761c0d3b7871143f558b41ca
DerickPascual <derickvanpascual@gmail.com>
2023-09-23 12:27:57 -0500

    Added router

--- src/index.js
+++ src/index.js
@@ -3,12 +3,39 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ArticlePage from './pages/ArticlePage/ArticlePage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import SignUpPage from './pages/SignUpPage/SignUpPage';
+import PreferencesPage from './pages/PreferencesPage/PreferencesPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <LoginPage />
+  },
+  {
+    path: '/sign-up',
+    element: <SignUpPage />
+  },
+  {
+    path: '/articles',
+    element: <ArticlePage />
+  },
+  {
+    path: '/preferences',
+    element: <PreferencesPage />
+  },
+  {
+    path: '/',
+    element: <App />
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
