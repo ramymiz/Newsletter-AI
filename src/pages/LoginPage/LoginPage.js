55965b4cebdef8a65c5db71f1d091258dacff194
Capn05 <89543708+Capn05@users.noreply.github.com>
2023-09-23 13:29:11 -0500

    Update LoginPage.js

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,45 +1,57 @@
-// Login.js
 import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+
 export default function LoginPage() {
-    const navigate = useNavigate();
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    const handleUsernameChange = (e) => {
-        setUsername(e.target.value);
-    };
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
 
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const handleLogin = () => {
-        // Implement your login logic here, e.g., make an API request
-    };
-    const handleSubmit = () => { 
-        navigate('/articles');
-    }
-    return (
-        <div>
-            <h2>Login</h2>
-            <form>
-                <div>
-                    <label>Username:</label>
-                    <TextField id="standard-basic" label="" variant="standard" />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <TextField id="standard-basic" label="" variant="standard" />
-                </div>
-                <Button variant="outlined" onClick={handleSubmit}>Submit</Button>
+  const handleLogin = () => {
+    // Implement your login logic here, e.g., make an API request
+  };
 
+  const handleSubmit = () => {
+    navigate('/articles');
+  };
 
-            </form>
-        </div>
-    );
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <h2>Login</h2>
+      <form style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <TextField
+          id="outlined-Username-input"
+          label="Username"
+          type="Username"
+          autoComplete="current-Username"
+          variant="outlined"
+          style={{ marginBottom: '16px', width: '300px' }}
+          value={username}
+          onChange={handleUsernameChange}
+        />
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="outlined"
+          style={{ marginBottom: '16px', width: '300px' }}
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <Button variant="outlined" onClick={handleSubmit} style={{ width: '300px' }}>
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
 }
-
-
