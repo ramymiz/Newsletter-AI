4d46ee6a38fc397b29f952bdbb333ac7031a1f41
DerickPascual <derickvanpascual@gmail.com>
2023-09-23 14:24:31 -0500

    added header for each page

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ function App() {
 
   return (
     <div>
-      <Header />
     </div>
   );
 }
--- src/shared_components/Header/Header.js
+++ src/shared_components/Header/Header.js
@@ -1,12 +1,26 @@
-import PersonIcon from '@mui/icons-material/Person';
 import './Header.css';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function Header() {
+    const navigate = useNavigate();
+
+    const handleLogoClick = () => {
+        navigate('/login');
+    }
+
+    const handlePreferencesClick = () => {
+        navigate('/preferences');
+    }
+
     return (
-        <div className="Header">
-            <h2>News AI</h2>
-            <h3>Preferences</h3>
+        <div className='Header'>
+            <button className="Header-logo-button" onClick={handleLogoClick}>
+                <h2>News AI</h2>
+            </button>
+            <button className="Header-preferences-button" onClick={handlePreferencesClick}>
+                <h3>Preferences</h3>
+            </button>
         </div>
     )
 }
