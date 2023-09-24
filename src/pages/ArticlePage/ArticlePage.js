f6052f8ba0c0589a57702b6a4fd498767cfa5bbb
DerickPascual <derickvanpascual@gmail.com>
2023-09-24 00:38:59 -0500

    s

--- src/pages/ArticlePage/ArticlePage.js
+++ src/pages/ArticlePage/ArticlePage.js
@@ -13,41 +13,41 @@ const articles = [
     summary: "Google is experimenting with an AI-powered product called Genesis for generating news articles. It has been demonstrated to media giants like The New York Times, The Washington Post, and News Corp (owner of The Wall Street Journal). While Google sees it as a tool to aid journalists by automating tasks, some executives are uneasy, feeling it may undermine quality journalism. Google insists that AI tools are meant to assist, not replace journalists, suggesting headlines and writing styles. Experts like journalism professor Jeff Jarvis see potential, but concerns exist about AI-generated content's nuances and its impact on trust in journalism. AI's use in newsrooms is a debated topic, with some outlets already using it for certain stories, raising concerns about misinformation and the perception of traditional journalism as AI continues to evolve.",
     bigSummary: "Google",
     image: image2,
-    tags: ["Software Engineering", "Finance"]
+    tags: ["Software Engineering", "Data Science"]
   },
   {
     title: "Article 2",
     summary: "Summary for article 2...",
     bigSummary: "bigSummary for article 2",
     image: image2,
-    tags: ["Software Engineering", "Finance"]
+    tags: ["Software Engineering", "Environmental Law"]
   },
   {
     title: "Google Tests A.I. Tool That Is Able to Write News Articles",
     summary: "Google is experimenting with an AI-powered product called Genesis for generating news articles. It has been demonstrated to media giants like The New York Times, The Washington Post, and News Corp (owner of The Wall Street Journal). While Google sees it as a tool to aid journalists by automating tasks, some executives are uneasy, feeling it may undermine quality journalism. Google insists that AI tools are meant to assist, not replace journalists, suggesting headlines and writing styles. Experts like journalism professor Jeff Jarvis see potential, but concerns exist about AI-generated content's nuances and its impact on trust in journalism. AI's use in newsrooms is a debated topic, with some outlets already using it for certain stories, raising concerns about misinformation and the perception of traditional journalism as AI continues to evolve.",
     bigSummary: "bigSummary for article 2",
     image: image2,
-    tags: ["Software Engineering", "Finance"]
+    tags: ["Climate Change Mitigation", "Natural Gas Distribution"]
   },
   {
     title: "Article 2",
     summary: "Summary for article 2...",
     bigSummary: "bigSummary for article 2",
     image: image2,
-    tags: ["Software Engineering", "Finance"]
+    tags: ["Environmental Law", "Algorithm Design", "Web Dev"]
   }, {
     title: "Google Tests A.I. Tool That Is Able to Write News Articles",
     summary: "Google is experimenting with an AI-powered product called Genesis for generating news articles. It has been demonstrated to media giants like The New York Times, The Washington Post, and News Corp (owner of The Wall Street Journal). While Google sees it as a tool to aid journalists by automating tasks, some executives are uneasy, feeling it may undermine quality journalism. Google insists that AI tools are meant to assist, not replace journalists, suggesting headlines and writing styles. Experts like journalism professor Jeff Jarvis see potential, but concerns exist about AI-generated content's nuances and its impact on trust in journalism. AI's use in newsrooms is a debated topic, with some outlets already using it for certain stories, raising concerns about misinformation and the perception of traditional journalism as AI continues to evolve.",
     bigSummary: "bigSummary for article 2",
     image: image2,
-    tags: ["Software Engineering", "Finance"]
+    tags: ["Environmental Policy", "Environmental Law"]
   },
   {
     title: "Article 2",
     summary: "Summary for article 2...",
     bigSummary: "bigSummary for article 2",
     image: image2,
-    tags: ["Software Engineering", "Finance"]
+    tags: ["Software Engineering", "Computer Science"]
   }];
 
 
